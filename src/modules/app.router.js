f519d982431dd7549e90167f237d05aa05ecd9ad
haneen <haneenemail@gmail.com>
2024-03-08 23:12:13 +0200

    update file

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -3,10 +3,22 @@ import authRouter from './auth/auth.router.js'
 import Adminrouter from './admin/admin.router.js';
 import voteRouter from './vote/vote.router.js';
 import candidateRouter from './candidate/candidate.router.js';
-
+import cors from 'cors';
 import { globalErrorHandler } from '../utls/errorHanding.js';
-
 const initApp = async (app, express) => {
+    /*
+app.use(async(req,res,next)=>{
+    let whitelist=['http://www.haneen.com'];
+    if(!whitelist.includes(req.header('origin'))){
+        return next (new Error(`invalid`,{cause:403}));
+    }
+    else{
+        next();
+    }
+})
+*/
+
+    app.use(cors());
     app.use(express.json());
     connectDB();
     app.get('/', (req, res) => {
