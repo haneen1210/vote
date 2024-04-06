cc86f6585592a66b97161195c26a3696629f3b18
haneen <haneenemail@gmail.com>
2024-04-06 03:57:53 +0300

    uplod file

--- src/modules/post/service/comment.service.js
+++ src/modules/post/service/comment.service.js
@@ -6,7 +6,7 @@ export const createComment = async (req, res, next) => {
     req.body.postId=req.params.id;
     req.body.userId=req.user._id;
     req.body.userName=req.user.userName;
-    req.body.image=req.user.image;
+ 
     const post=await PostModel.findById(req.params.id);
     if(!post){
         return next(new Error(`invalid post id`));
