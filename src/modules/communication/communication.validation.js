e04fc46629b9e3803989f51e02069964c2ce0e42
haneen <haneenemail@gmail.com>
2024-04-21 00:13:57 +0300

    uplod file

--- src/modules/communication/communication.validation.js
+++ src/modules/communication/communication.validation.js
@@ -21,8 +21,14 @@ export const getspecificcommunication=joi.object({
 
 export const updatecommunicationSchema=joi.object({
     id:joi.string().min(24).max(24).required(),
-    type: joi.string().valid('email', 'whatsapp', 'facebook', 'instagram').required(),
+    socialMedia: joi.array().items(joi.object({
+        image: joi.object({
+            url: joi.string().required().uri() // Validate image URL as URI
+        }).required(),
+        link: joi.string().required().uri().min(10), // Validate link as URI with minimum length of 10 characters
+    })).required(),
     phone: joi.string().required().min(10).max(10),
-    address: joi.string().required(),
+    address: joi.string().required().min(3),
     logo: joi.string().uri(),
+   email: generalFields.email,
 });
