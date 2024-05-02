edf55b289ed42e3b7c8fcf9d9403908f75687a77
haneen <haneenemail@gmail.com>
2024-05-02 14:23:55 +0300

    uplod file

--- src/modules/vote/vote.service.js
+++ src/modules/vote/vote.service.js
@@ -101,7 +101,7 @@ export const getVoteOpen = async (req, res, next) => {
 export const getpreviousvotes = async (req, res, next) => {
     const votes = await voteModel.find({ VotingStatus: "Inactive",EndDateVote: { $lt: new Date() }  });
     return res.status(200).json({ message: "success", votes });
-  };/*
+  };
 export const getspecificVote = async (req, res) => {
   const { id } = req.params;
   const vote = await voteModel.findById(id);
@@ -113,8 +113,8 @@ export const getspecificVote = async (req, res) => {
   });
   return res.status(200).json({ message: "success", subvote });
 };
-*/
 
+/*
 export const getspecificVote = async (req, res) => {
   const { id } = req.params;
     const vote = await voteModel.findById(id).populate({
@@ -131,6 +131,7 @@ export const getspecificVote = async (req, res) => {
   
   }
 ;
+*/
 export const getallVoteandcatecory = async (req, res) => {
   const subvote = await voteModel.find().populate({
     path: "candidates",
