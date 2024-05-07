8d842509d52b3230870a1ff9a442d68f471892a5
haneen <haneenemail@gmail.com>
2024-05-07 21:34:38 +0300

    uplod file

--- src/modules/admin/admin.validation.js
+++ src/modules/admin/admin.validation.js
@@ -2,7 +2,7 @@ import joi from "joi";
 import { generalFields } from "../../middleware/validation.js";
 
 export const UpdateSchema = joi.object({
-    id:joi.string().min(24).max(24).required(),
+    //id:joi.string().min(24).max(24).required(),
     email: joi.string().email(),
     userName: joi.string().alphanum().min(3).max(25),
     phone: joi.string().min(10).max(10),
