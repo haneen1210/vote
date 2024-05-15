1e3f9dcc4c04353ee717bcda97ccf67ea26783eb
haneen <haneenemail@gmail.com>
2024-05-15 20:16:07 +0300

    uplod file

--- src/modules/candidate/candidate.router.js
+++ src/modules/candidate/candidate.router.js
@@ -8,12 +8,12 @@ import * as validators from './candidate.validation.js';
 
 
 const router = Router();
-router.get('/AllVotesParticipateIn',auth(Object.values(roles)),asynHandler(candidateservices.getspecificCandidateinvotes));
-router.get('/getCandidatePosts',auth(Object.values(roles)),asynHandler(candidateservices.getCandidatePosts));
+router.get('/AllVotesParticipateIn',auth(endPoint.AllVotesParticipateIn),asynHandler(candidateservices.getspecificCandidateinvotes));
+router.get('/getCandidatePosts',auth(endPoint.AllVotesParticipateIn),asynHandler(candidateservices.getCandidatePosts));
 router.get('/getcandidate', auth(endPoint.getcandidate), asynHandler(candidateservices.getcandidate));
+router.post('/requestWithdrawal', auth(endPoint.requestWithdrawal), validation(validators.requestWithdrawal),asynHandler(candidateservices.requestWithdrawal));
 router.get('/:CandidateID', auth(endPoint.getspecificCandidate), validation(validators.getspecificCandidate), asynHandler(candidateservices.getspecificCandidateinvote));
 router.get('/CandidateIDvalid/:CandidateID', auth(endPoint.getspecificCandidate), validation(validators.getspecificCandidate), asynHandler(candidateservices.getspecificCandidate));
-router.post('/requestWithdrawal', auth(endPoint.requestWithdrawal), validation(validators.requestWithdrawal),asynHandler(candidateservices.requestWithdrawal));
 
 export default router;
 //auth(Object.values(roles))
