583f66ba491469c80cfd2ed21727f42bc77a62d7
haneen <haneenemail@gmail.com>
2024-05-15 20:22:58 +0300

    uplod file

--- src/modules/candidate/candidate.router.js
+++ src/modules/candidate/candidate.router.js
@@ -12,8 +12,8 @@ router.get('/AllVotesParticipateIn',auth(endPoint.AllVotesParticipateIn),asynHan
 router.get('/getCandidatePosts',auth(endPoint.AllVotesParticipateIn),asynHandler(candidateservices.getCandidatePosts));
 router.get('/getcandidate', auth(endPoint.getcandidate), asynHandler(candidateservices.getcandidate));
 router.post('/requestWithdrawal', auth(endPoint.requestWithdrawal), validation(validators.requestWithdrawal),asynHandler(candidateservices.requestWithdrawal));
-router.get('/:CandidateID', auth(endPoint.getspecificCandidate), validation(validators.getspecificCandidate), asynHandler(candidateservices.getspecificCandidateinvote));
 router.get('/CandidateIDvalid/:CandidateID', auth(endPoint.getspecificCandidate), validation(validators.getspecificCandidate), asynHandler(candidateservices.getspecificCandidate));
+router.get('/:CandidateID', auth(endPoint.getspecificCandidate), validation(validators.getspecificCandidate), asynHandler(candidateservices.getspecificCandidateinvote));
 
 export default router;
 //auth(Object.values(roles))
