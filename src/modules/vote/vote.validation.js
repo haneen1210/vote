5d8721697921f1fe55802e510c6d3724d5e2e72f
haneen <haneenemail@gmail.com>
2024-05-16 15:50:03 +0300

    uplod file

--- src/modules/vote/vote.validation.js
+++ src/modules/vote/vote.validation.js
@@ -1,6 +1,6 @@
 import joi from "joi";
 import { generalFields, validation } from "../../middleware/validation.js";
-
+/*
 
 const datePattern = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
 export const createVoteSchema = joi.object({
@@ -18,6 +18,26 @@ export const createVoteSchema = joi.object({
     file: generalFields.file.required(),
     AdminID:joi.string().min(24).max(24).required(),
 });
+*/
+const datePattern = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
+
+export const createVoteSchema = joi.object({
+    voteName: joi.string().min(3).max(25).required(),
+    VotingStatus: joi.string().valid('Active', 'Inactive').required(),
+    description: joi.string().min(10).max(100).required(),
+    StartDateVote: joi.string().pattern(datePattern).required().custom((value, helpers) => {
+        const date = moment(value, 'MM/DD/YYYY hh:mm A').toDate();
+        return helpers.date.date = date;
+    }),
+    EndDateVote: joi.string().pattern(datePattern).required().custom((value, helpers) => {
+        const date = moment(value, 'MM/DD/YYYY hh:mm A').toDate();
+        return helpers.date.date = date;
+    }),
+    file: generalFields.file.required(),
+    AdminID: joi.string().min(24).max(24).required(),
+});
+
+
 
 export const updateVoteSchema = joi.object({
     id: joi.string().custom(validation).required(),
