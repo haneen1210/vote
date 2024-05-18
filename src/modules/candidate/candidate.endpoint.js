1884512271034ae592d7f89e24c346c6fd0daf43
haneen <haneenemail@gmail.com>
2024-05-18 21:30:35 +0300

    uplod file

--- src/modules/candidate/candidate.endpoint.js
+++ src/modules/candidate/candidate.endpoint.js
@@ -1,7 +1,7 @@
 import { roles } from "../../middleware/auth.js"
 
 export const endPoint = {
-    getspecificCandidate:[roles.SuperAdmin,oles.Admin],
+    getspecificCandidate:[roles.SuperAdmin,roles.Admin],
     getcandidate:[roles.SuperAdmin,roles.Admin],
     AllVotesParticipateIn:[roles.SuperAdmin,roles.Candidate],
     requestWithdrawal:[roles.SuperAdmin,roles.Candidate],
