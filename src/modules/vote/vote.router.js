9fa337e23b063e9dc743795b8b4a0b7cea9037f7
haneen <haneenemail@gmail.com>
2024-05-18 22:11:23 +0300

    uplod file

--- src/modules/vote/vote.router.js
+++ src/modules/vote/vote.router.js
@@ -22,6 +22,7 @@ router.get('/user-votes',auth(endPoint.getUserVotes), asynHandler(voteservices.g
 router.post('/addcandidatetovote', auth(endPoint.addExistingCandidateToVote), validation(validators.addAndRemoveCandidateToVote), asynHandler(voteservices.addExistingCandidateToVote));
 router.post('/uploadExcelCandidateToVote', auth(endPoint.addExistingCandidateToVote), fileUpload(fileValidation.excel).single('file'), asynHandler(voteservices.uploadExcelCandidateToVote));
 router.patch('/removeCandidateFromVote',auth(endPoint.removeCandidateFromVote),validation(validators.addAndRemoveCandidateToVote), asynHandler(voteservices.removeCandidateFromVote));
+router.post('/addExistingUserToVote', auth(endPoint.addExistingCandidateToVote), validation(validators.addAndRemoveCandidateToVote), asynHandler(voteservices.addExistingUserToVote));
 router.get('/getvotesadmin', auth(endPoint.getAdminvote), asynHandler(voteservices.getVotesByAdmin));
 router.patch('/updateVotingStatus/:id', auth(endPoint.updateVotingStatus), validation(validators.updateVoteSchema), asynHandler(voteservices.updateVotingStatus));
 router.get('/getvotes/:AdminID', auth(endPoint.SuperAdmin), asynHandler(voteservices.getVotesByIDAdmin));
